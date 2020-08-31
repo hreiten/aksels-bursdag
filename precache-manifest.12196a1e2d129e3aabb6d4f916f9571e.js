8594f629bc470c9af4f175ac3ebd27f4042f846f
Halvor Reiten <ha.reiten@gmail.com>
2020-08-31 15:07:43 +0200

    Updates

---
+++ precache-manifest.12196a1e2d129e3aabb6d4f916f9571e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3b4fe2a748033dcb1acb26dc4c2382",
+    "url": "/aksels-bursdag/index.html"
+  },
+  {
+    "revision": "3a22c00836504897e1ae",
+    "url": "/aksels-bursdag/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14aa242d49d75c20a027",
+    "url": "/aksels-bursdag/static/js/2.022ac952.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aksels-bursdag/static/js/2.022ac952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a22c00836504897e1ae",
+    "url": "/aksels-bursdag/static/js/main.90fc2ec2.chunk.js"
+  },
+  {
+    "revision": "cbdbeea027c846e8c1a2",
+    "url": "/aksels-bursdag/static/js/runtime-main.0ff20785.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aksels-bursdag/static/media/logo.5d5d9eef.svg"
+  }
+]);
