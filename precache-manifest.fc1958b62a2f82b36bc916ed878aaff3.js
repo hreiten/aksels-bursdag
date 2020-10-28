1a7c94fe41e2efb862eb957315dffb084a31b1af
Halvor Reiten <ha.reiten@gmail.com>
2020-10-28 09:45:37 +0100

    Updates

---
+++ precache-manifest.fc1958b62a2f82b36bc916ed878aaff3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef4b824231dc84f88d97b0a2a7af608",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd17aa196673617a0301",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e0abe71dd9324f4edbf",
+    "url": "/static/js/2.1ecc5004.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ecc5004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd17aa196673617a0301",
+    "url": "/static/js/main.a7de4165.chunk.js"
+  },
+  {
+    "revision": "bdbc2ad01d965f59d985",
+    "url": "/static/js/runtime-main.f1095742.js"
+  },
+  {
+    "revision": "c709466019babd43194ae7bf4afcb5ef",
+    "url": "/static/media/branca.c7094660.png"
+  },
+  {
+    "revision": "64992bf53811d1bde6fa1292db97dfdf",
+    "url": "/static/media/extra.64992bf5.png"
+  },
+  {
+    "revision": "159e5d42b7f8f266ccd4d6c8779007bd",
+    "url": "/static/media/face.159e5d42.png"
+  },
+  {
+    "revision": "7cb5ebe4804d177c79d883b3857abac0",
+    "url": "/static/media/hooka.7cb5ebe4.png"
+  },
+  {
+    "revision": "c0b28df4128025af005ba5842b8050de",
+    "url": "/static/media/os.c0b28df4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aksels-bursdag/precache-manifest.614772ebf0df4a7029af9d76e4f9ea53.js"
+  "/precache-manifest.fc1958b62a2f82b36bc916ed878aaff3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aksels-bursdag/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
